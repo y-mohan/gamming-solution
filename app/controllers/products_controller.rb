4bba1c95be91bf6d2fba23daccf3e669fd91d79d
yellappamohan <yellappamohan1@gmail.com>
2017-05-09 08:52:51 +0530

    prodcuts with multiple images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,34 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user, only: [:index]
   def index
+  	@product = Product.all
   end
 
   def new
+  	@product = Product.new
   end
-
+  def create
+  	 image_attributes = product_params[:image]["photo"]
+     product_params.delete(:image)
+     @product = Product.new(product_params)
+     if @product.save
+        image_attributes.each do |each_image|
+          image = Image.new(photo: each_image, product: @product)
+          image.save(validate: false)
+        end
+        redirect_to products_path
+        else
+          render :new
+     end
+  end
   def edit
   end
 
   def show
+  end
+
+  private
+  def product_params
+  	params.require(:product).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ end
        delete 'logout'
   	end
   end
-  root to: "users#index" 
+  root to: "products#index" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
